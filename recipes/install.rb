b710b5c5e2000bda1102c7753f018351958d5462
archit2693 <archit2693.aj@gmail.com>
2018-06-29 14:36:01 +0530

    add apt_repository to install ruby

---
+++ recipes/install.rb
@@ -0,0 +1,7 @@
+app_name = 'Deploy'
+release_file = Github.new('ritik02/Deploy').get_release_file
+release_name = Github.new('ritik02/Deploy').get_release_name
+
+apt_repository 'brightbox-ruby' do
+  uri 'ppa:brightbox/ruby-ng'
+end
--- spec/unit/recipes/install_spec.rb
+++ spec/unit/recipes/install_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe 'test::install' do
-  
+
+  before(:each) do
+    @runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
+    end
+    allow_any_instance_of(Chef::HTTP).to receive(:get).and_return("{\"tag_name\":\"V1.0\"}")
+  end
+	context 'apt_repository' do
+    it 'should add brightbox/ruby-ng in apt-rep' do
+      chef_run = @runner.converge(described_recipe)
+      expect(chef_run).to add_apt_repository('brightbox-ruby')
+    end
+  end  
 end
