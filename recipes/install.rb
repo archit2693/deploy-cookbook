4254ddc20640b451fab45f265c2de4ddb5eb3c13
archit2693 <archit2693.aj@gmail.com>
2018-06-29 14:42:02 +0530

    add apt_get update

--- recipes/install.rb
+++ recipes/install.rb
@@ -5,3 +5,7 @@ release_name = Github.new('ritik02/Deploy').get_release_name
 apt_repository 'brightbox-ruby' do
   uri 'ppa:brightbox/ruby-ng'
 end
+
+apt_update 'update' do
+  action :update
+end
--- spec/unit/recipes/install_spec.rb
+++ spec/unit/recipes/install_spec.rb
@@ -3,14 +3,22 @@ require 'spec_helper'
 describe 'test::install' do
 
   before(:each) do
-    @runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
+    @chef_runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
     end
     allow_any_instance_of(Chef::HTTP).to receive(:get).and_return("{\"tag_name\":\"V1.0\"}")
   end
+
 	context 'apt_repository' do
     it 'should add brightbox/ruby-ng in apt-rep' do
-      chef_run = @runner.converge(described_recipe)
-      expect(chef_run).to add_apt_repository('brightbox-ruby')
+      response = @chef_runner.converge(described_recipe)
+      expect(response).to add_apt_repository('brightbox-ruby')
     end
   end  
+
+  context 'apt_update' do
+    it 'updates apt repo' do
+      response = @chef_runner.converge(described_recipe)
+      expect(response).to update_apt_update('update')
+    end
+  end
 end
